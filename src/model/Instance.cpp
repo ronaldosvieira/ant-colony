0f17fa15fdc45d6b9f69d319dd23dc5e367b3d51
Ronaldo Vieira <ronaldoesv@gmail.com>
2015-11-29 15:11:25 -0200

    Leitura de instâncias de arquivos

--- src/model/Instance.cpp
+++ src/model/Instance.cpp
@@ -15,10 +15,84 @@ Instance::Instance(int &numItems, int &numKnapsacks, std::vector<int> &weightLis
 	if (profitList.size() != numItems) throw std::invalid_argument("Size of profit list not equal to numItems");
 	if (weightList.size() != numItems) throw std::invalid_argument("Size of weight list not equal to numItems");
 	if (capacityList.size() != numKnapsacks) throw std::invalid_argument("Size of capacity list not equal to numKnapsacks");
+}
 
+Instance::Instance(const std::string fileName) {
+	std::ifstream file;
+	std::string line;
+	std::vector<std::string> tokens;
+
+	file.open(fileName.c_str());
+	if (!file.is_open()) throw std::invalid_argument("Couldn't open file" + fileName);
+
+	// read numItems and numKnapsacks
+	getline(file, line);
+	split(line, ' ', tokens);
+
+	this->numItems 		= atoi(tokens.at(1).c_str());
+	this->numKnapsacks 	= atoi(tokens.at(0).c_str());
+
+	line.clear();
+	tokens.clear();
+
+	// read profitList
+	getline(file, line);
+	split(line, ' ', tokens);
+
+	for (int i = 0; i < tokens.size(); ++i) {
+		this->profitList.push_back(atoi(tokens.at(i).c_str()));
+	}
+
+	if (this->profitList.size() != numItems) {
+		throw std::invalid_argument("Size of profit list not equal to numItems");
+	}
+
+	line.clear();
+	tokens.clear();
+
+	// read capacityList
+	getline(file, line);
+	split(line, ' ', tokens);
+
+	for (int i = 0; i < tokens.size(); ++i) {
+		capacityList.push_back(atoi(tokens.at(i).c_str()));
+	}
+
+	if (this->capacityList.size() != numKnapsacks) {
+		throw std::invalid_argument("Size of capacity list not equal to numKnapsacks");
+	}
+
+	line.clear();
+	tokens.clear();
+
+	// read weightList
+	getline(file, line);
+	split(line, ' ', tokens);
+
+	for (int i = 0; i < tokens.size(); ++i) {
+		weightList.push_back(atoi(tokens.at(i).c_str()));
+	}
+
+	if (this->weightList.size() != numItems) {
+		throw std::invalid_argument("Size of weight list not equal to numItems");
+	}
+
+	file.close();
 }
 
 Instance::~Instance() {
 	// TODO Auto-generated destructor stub
 }
 
+void Instance::split(const std::string& s, char delim, std::vector<std::string>& v) {
+    auto i = 0;
+    auto pos = s.find(delim);
+    while (pos != std::string::npos) {
+      v.push_back(s.substr(i, pos-i));
+      i = ++pos;
+      pos = s.find(delim, pos);
+
+      if (pos == std::string::npos)
+         v.push_back(s.substr(i, s.length()));
+    }
+}
--- src/model/Instance.h
+++ src/model/Instance.h
@@ -9,7 +9,11 @@
 #define INSTANCE_H_
 
 #include <vector>
+#include <string>
+#include <iostream>
+#include <cstdlib>
 #include <stdexcept>
+#include <fstream>
 
 class Instance {
 public:
@@ -21,7 +25,10 @@ public:
 
 	Instance(int &numItems, int &numKnapsacks, std::vector<int> &weightList,
 			std::vector<int> &profitList, std::vector<int> &capacityList);
+	Instance(std::string fileName);
 	virtual ~Instance();
+private:
+	void split(const std::string& s, char delim, std::vector<std::string>& v);
 };
 
 #endif /* INSTANCE_H_ */

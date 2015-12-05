b12b1ab4265f9d76deceab3c5e9e0510a2b2d89d
Ronaldo Vieira <ronaldoesv@gmail.com>
2015-12-05 17:06:40 -0200

    Implementação da seleção de modo (test/run)

--- src/Runner.cpp
+++ src/Runner.cpp
@@ -19,6 +19,10 @@
 #include "test/AntTest.h"
 #include "test/ColonyTest.h"
 
+#include <iostream>
+
+constexpr auto TEST = 0;
+
 void runSuite(){
     cute::suite s;
 
@@ -31,6 +35,44 @@ void runSuite(){
     cute::makeRunner(lis)(s, "Instance class tests");
 }
 
+Instance openInstance(std::string fileName) {
+	try {
+		return Instance("./src/instances/" + fileName);
+	} catch (std::invalid_argument &e) {
+		std::cout << "Problem in instance " << fileName << ":\n";
+		std::cout << e.what() << "\n";
+	}
+}
+
+void run() {
+	Instance sento1 = openInstance("sento1.dat");
+	int numAnts = 100;
+
+	Colony col(sento1, numAnts, 0.5, 1.0, 2.0);
+	col.run();
+	std::vector<long> values = col.getSolutionValues();
+
+	int best = 0;
+	long sum = 0;
+	for (int i = 0; i < numAnts; ++i) {
+		//std::cout << "Ant " << i << ": " << values.at(i) << "\n";
+		if (values.at(i) > best) best = values.at(i);
+		sum += values.at(i);
+		std::cout << values.at(i) << " ";
+		if ((i + 1) % 20 == 0) std::cout << "\n";
+	}
+
+	std::cout << "\nBest: " << best << "\n";
+	std::cout << "Mean: " << sum / numAnts << "\n";
+
+}
+
 int main(int argc, char **argv) {
-	runSuite();
+	if (TEST) runSuite();
+	else {
+		try {run();}
+		catch (std::exception &e) {
+			std::cout << "deu merda: " << e.what() << "\n";
+		}
+	}
 }

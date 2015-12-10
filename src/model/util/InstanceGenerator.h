4e05e4f32c1501b9c79316f2e0274d1b672511ce
Ronaldo Vieira <ronaldoesv@gmail.com>
2015-12-10 17:15:04 -0200

    Correção da geração de instâncias fracamente correlacionadas

--- src/model/util/InstanceGenerator.h
+++ src/model/util/InstanceGenerator.h
@@ -9,7 +9,7 @@
 #define SRC_INSTANCES_INSTANCEGENERATOR_H_
 
 #define DEBUG_BUILD
-#undef DEBUG_BUILD
+//#undef DEBUG_BUILD
 
 #ifdef DEBUG_BUILD
 #define DEBUG(x) std::cout << x
@@ -24,14 +24,16 @@
 #include <iostream>
 
 constexpr auto UNIFORM_MIN = 10;
-constexpr auto UNIFORM_MAX = 100;
+constexpr auto UNIFORM_MAX = 1000;
+
+constexpr auto WEAKLY_TERM = (UNIFORM_MAX - UNIFORM_MIN)/10;
 
 class InstanceGenerator {
 public:
 	InstanceGenerator();
 	virtual ~InstanceGenerator();
 
-	static Instance generate(std::string fileName, int numItems, int numKnapsacks) {
+	static Instance generate(std::string fileName, int numItems, int numKnapsacks, int corr = 0, int similar = 0) {
 		rand(); rand();
 
 		std::vector<int> wL;
@@ -41,9 +43,21 @@ public:
 		DEBUG("Generating instance\n");
 		DEBUG("with i = " << numItems << " and k = " << numKnapsacks << "\n\n");
 
-		for (int i = 0; i < numItems; ++i) {
-			wL.push_back((rand() % (UNIFORM_MAX - UNIFORM_MIN)) + UNIFORM_MIN);
-			pL.push_back((rand() % (UNIFORM_MAX - UNIFORM_MIN)) + UNIFORM_MIN);
+		if (corr == 0) {
+			for (int i = 0; i < numItems; ++i) {
+				wL.push_back((rand() % (UNIFORM_MAX - UNIFORM_MIN)) + UNIFORM_MIN);
+				pL.push_back((rand() % (UNIFORM_MAX - UNIFORM_MIN)) + UNIFORM_MIN);
+			}
+		} else if (corr == 1) {
+			for (int i = 0; i < numItems; ++i) {
+				wL.push_back((rand() % (UNIFORM_MAX - UNIFORM_MIN)) + UNIFORM_MIN);
+				pL.push_back((rand() % ((wL.at(i) + WEAKLY_TERM) - (wL.at(i) - WEAKLY_TERM))) + fmax(UNIFORM_MIN, (wL.at(i) - WEAKLY_TERM)));
+			}
+		} else if (corr == 2) {
+			for (int i = 0; i < numItems; ++i) {
+				wL.push_back((rand() % (UNIFORM_MAX - UNIFORM_MIN)) + UNIFORM_MIN);
+				pL.push_back(wL.at(i) + WEAKLY_TERM);
+			}
 		}
 
 		DEBUG("wL: ");
@@ -62,27 +76,48 @@ public:
 
 		DEBUG("\n");
 
-		double capacityMinBound = UNIFORM_MIN;
-		double capacityMaxBound[numKnapsacks];
 		double weightListSum = 0.0;
 
 		for (int i = 0; i < numItems; ++i) {
 			weightListSum += wL.at(i);
 		}
 
-		DEBUG("cL: ");
+		if (similar == 0) {
+			double capacityMinBound = UNIFORM_MIN;
+			double capacityMaxBound[numKnapsacks];
 
-		for (int k = 0; k < numKnapsacks; ++k) {
-			capacityMaxBound[k] = 0.5 * weightListSum;
+			DEBUG("cL: ");
 
-			for (int k2 = 0; k2 < k; ++k2) {
-				capacityMaxBound[k] -= cL.at(k2);
+			for (int k = 0; k < numKnapsacks; ++k) {
+				capacityMaxBound[k] = 0.5 * weightListSum;
+
+				for (int k2 = 0; k2 < k; ++k2) {
+					capacityMaxBound[k] -= cL.at(k2);
+				}
+
+				cL.push_back(rand() % ((int) (capacityMaxBound[k] - capacityMinBound)) + capacityMinBound);
+
+				DEBUG(cL.at(k) << " ");
+			}
+		} else if (similar == 1) {
+			double capacityMinBound = 0.4 * (weightListSum / numKnapsacks);
+			double capacityMaxBound = 0.6 * (weightListSum / numKnapsacks);
+			double lastCapacity = 0.5 * weightListSum;
+
+			DEBUG("cL: ");
+
+			for (int k = 0; k < numKnapsacks - 1; ++k) {
+				cL.push_back((rand() % ((int) (capacityMaxBound - capacityMinBound))) + capacityMinBound);
+				lastCapacity -= cL.at(k);
+
+				DEBUG(cL.at(k) << " ");
 			}
 
-			cL.push_back(rand() % ((int) (capacityMaxBound[k] - capacityMinBound)) + capacityMinBound);
+			cL.push_back((int) lastCapacity);
 
-			DEBUG(cL.at(k) << " ");
+			DEBUG(cL.at(numKnapsacks - 1) << " ");
 		}
+
 		DEBUG("\n\n");
 
 		Instance result(numItems, numKnapsacks, wL, pL, cL);
